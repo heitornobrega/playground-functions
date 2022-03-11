b9a85819bb3ac3b23cbe459901f51988eaec3c34
Heitor Nóbrega Tico <nobheitor@gmail.com>
2022-03-11 14:58:36 -0300

    create function compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,9 @@
-// Desafio 01 
-function compareTrue() {
-  // seu código aqui
+// Desafio 1 
+function compareTrue(v1, v2) {
+  if(v1 === true && v2 === true){
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
