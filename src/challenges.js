c2422faeefe929275a5f7019db05c05cbfced3a4
Heitor Nóbrega Tico <nobheitor@gmail.com>
2022-03-12 17:26:46 -0300

    create encode and decode method's

--- src/challenges.js
+++ src/challenges.js
@@ -90,12 +90,29 @@ function fizzBuzz(numeros) {
 
 // Desafio 9
 function encode(texto) {
-  for (let c in texto) {
-
-  }
+  var vogais = ["a", "e", "i", "o", "u"]
+  for (var c in texto) {
+    for (i in vogais) {
+        if (texto[c] === vogais[i]) {
+          i++
+          texto = texto.replace(texto[c], i.toString());
+        };
+    }
 }
-function decode() {
-  // seu código aqui
+return texto;
+}
+
+function decode(texto) {
+  var vogais = ["a", "e", "i", "o", "u"];
+  var numeros = ["1", "2", "3", "4", "5"];
+  for (c in texto) {
+    for (i in numeros) {
+      if(texto[c] === numeros[i]){
+        texto = texto.replace(texto[c], vogais[i]);
+      }
+    }
+  }
+  return texto;
 }
 
 // Desafio 10
